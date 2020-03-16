83b6dc19e1fded4321f04c8aaa7e4570323fcc04
georgebyron95 <piero.lafiosca95@gmail.com>
2020-03-16 17:38:59 +0100

    MILO - hubei to be fixed

--- model-MILO/fit_hubei/fit_self_hubei.py
+++ model-MILO/fit_hubei/fit_self_hubei.py
@@ -9,7 +9,7 @@ def check_boundary(data, abitanti):
         data = abitanti
     return data
 
-def integrate_ode(x, tempo_totale):
+def integrate_ode(x, tempo_totale, quarantine):
     # variabili importanti
     h = 5.85*(10**7) # numero di abitanti
     r = 74 # 18.5*4 # tempo medio di guarigione, per 4 perche' aggiorniamo ogni 6 ore
@@ -18,6 +18,9 @@ def integrate_ode(x, tempo_totale):
     I = [] # lista degli infetti
     R = [] # lista dei guariti
     S = []
+    delta_SE = []
+    delta_EI = []
+    delta_IR = []
     s = x[0] # parametro s
     alpha = x[1]
     # condizione iniziale
@@ -27,39 +30,33 @@ def integrate_ode(x, tempo_totale):
     S.append(h-1) # numero di sani
     # inizia l'integrazione numerica
     for step in range(tempo_totale):
-        # aggiorno R
-        if step-r < 0: # controlliamo se qualcuno e' gia' guarito
-            new_R = R[step]
+        print("SITUA: "+str(S[step])+" "+str(E[step])+" "+str(I[step])+" "+str(R[step]))
+        if quarantine:
+            if I[step] > 1000:
+                alpha = 5
+        # calcolo delta SE
+        delta_SE.append((1-(1-s/(alpha*h))**(I[step]/alpha))*S[step])
+        qua   = (1-(1-s/(alpha*h))**(I[step]/alpha))*S[step]
+        noqua = (1-(1-s/h)**(I[step]))*S[step]
+        print("alpha "+str(alpha)+" qua "+str(qua)+ " noqua "+str(noqua))
+        # calcolo delta EI
+        if step - t < 0: # se non e' passato un ciclo di incubazione e' uguale a zero
+            delta_EI.append(0)
         else:
-            #new_R = I[step+1-r]+R[step]
-            new_R = I[step+1-r]-I[step-r]+R[step]
-        new_R = check_boundary(new_R, h)
-        R.append(new_R)
-        # aggiorno I
-        if step-t < 0: # controlliamo se e' gia' passato un ciclo di incubazione
-            new_I = I[step]-(R[step+1]-R[step])
+            delta_EI.append(delta_SE[step-t])
+        # calcolo delta IR
+        if step - r < 0: # se non e' passato un ciclo di guarigione e' uguale a 0
+            delta_IR.append(0)
         else:
-            #new_I = E[step+1-t]+I[step]-(R[step+1]-R[step])
-            new_I = E[step+1-t]-E[step-t]+I[step]-(R[step+1]-R[step])
-        new_I = check_boundary(new_I, h)
-        I.append(new_I)
-        # aggiorno E
-        #new_E = E[step] - (I[step+1]-I[step]) + (1-(1-s*alpha/h)**(I[step]/alpha))*(h-E[step]-I[step]-R[step])
-        print("TEMPO "+str(step))
-        print("vecchi exposed    : "+str(E[step]))
-        print("nuovi infetti     : "+str(I[step+1]-I[step]))
-        print("nuovi contagiabili: "+str(S[step]))
-        print("nuovi contagiati  : "+str((1-(1-s*alpha/h)**(I[step]/alpha))*S[step]))
-        new_E = E[step] - (I[step+1]-I[step]) + (1-(1-s*alpha/h)**(I[step]/alpha))*S[step]
-        print("nuovi exposed     : "+str(new_E))
-        new_E = check_boundary(new_E, h)
-        E.append(new_E)
-        # aggiorno S
-        new_S = h - E[step+1] - I[step+1] - R[step+1]
-        new_S = check_boundary(new_S, h)
-        S.append(new_S)
-        print("tempo "+str(step))
-        print(str(new_S)+" "+str(new_E)+" "+str(new_I)+" "+str(new_R))
+            delta_IR.append(delta_EI[step-r])
+        # print
+        #print("stampa dei delta")
+        #print("DELTA "+str(alpha)+" "+str(delta_SE[step])+" "+str(delta_EI[step])+" "+str(delta_IR[step]))
+        # aggiorno le quantita
+        S.append(S[step]-delta_SE[step])
+        E.append(E[step]-delta_EI[step]+delta_SE[step])
+        I.append(I[step]-delta_IR[step]+delta_EI[step])
+        R.append(R[step]+delta_IR[step])
 
     # fine integrazione numerica
     return E, I, R, S
@@ -70,7 +67,7 @@ def calc_least_squares(x, tempo_totale):
     I = []
     R = []
     S = []
-    E, I, R, S = integrate_ode(x, tempo_totale)
+    E, I, R, S = integrate_ode(x, tempo_totale, False)
     h = 5.85*(10**7) # numero di abitanti
     #print(E)
     #print(I)
@@ -93,7 +90,37 @@ def calc_least_squares(x, tempo_totale):
     #for spine in ('top', 'right', 'bottom', 'left'):
     #    ax.spines[spine].set_visible(False)
     
-    plt.title ('MILO')
+    plt.title ('MILO - no quarantine')
+#    plt.savefig('model_SIR.png', dpi = 300)
+    plt.show()
+    E = []
+    I = []
+    R = []
+    S = []
+    E, I, R, S = integrate_ode(x, tempo_totale, True)
+    h = 5.85*(10**7) # numero di abitanti
+    #print(E)
+    #print(I)
+    #print(R)
+    fig = plt.figure(facecolor='w')
+    t = np.linspace(0, tempo_totale, tempo_totale+1)
+    #ax = fig.add_subplot(111, axis_bgcolor='#dddddd', axisbelow=True)
+    plt.plot(t, np.array(S)/h, 'b', alpha=0.5, lw=2, label='Fraz. Sani')
+    plt.plot(t, np.array(E)/h, 'r', alpha=0.5, lw=2, label='Fraz. Esposti')
+    plt.plot(t, np.array(I)/h, 'g', alpha=0.5, lw=2, label='Fraz. Infetti')
+    plt.plot(t, np.array(R)/h, 'y', alpha=0.5, lw=2, label='Fraz. Guariti')
+    plt.xlabel('Tempo [6 ore]')
+    plt.ylabel('Frazione')
+    #plt.ylim(0,1.2)
+    #ax.yaxis.set_tick_params(length=0)
+    #ax.xaxis.set_tick_params(length=0)
+    #ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+    legend = plt.legend()
+    legend.get_frame().set_alpha(0.8)
+    #for spine in ('top', 'right', 'bottom', 'left'):
+    #    ax.spines[spine].set_visible(False)
+    
+    plt.title ('MILO - quarantine')
 #    plt.savefig('model_SIR.png', dpi = 300)
     plt.show()
 
@@ -104,4 +131,4 @@ def calc_least_squares(x, tempo_totale):
    # return sum_squares
 
 #print(calc_least_squares(np.array([3,1]),tuple(ref)))
-calc_least_squares(np.array([3,1]),180)
+calc_least_squares(np.array([1.1,1]),2000)

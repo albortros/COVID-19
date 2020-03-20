0e01bc0f59f8c41eba1409d4bed751c7242a3720
Giacomo Petrillo <info@giacomopetrillo.com>
2020-03-20 14:18:23 +0100

    added removed cases to plot

--- predictions/plot-dati-regioni.py
+++ predictions/plot-dati-regioni.py
@@ -22,8 +22,10 @@ regions = data['denominazione_regione'].unique()
 # Prepare figure.
 fig = plt.figure('predictions-plot')
 fig.clf()
-fig.set_size_inches((8, 4))
-axs = fig.subplots(1, 2, sharex=True, sharey=True)
+fig.set_size_inches((12, 4))
+axs = fig.subplots(1, 3, sharex=True)
+labels = ['total', 'infected', 'removed']
+axsl = {l: a for l, a in zip(labels, axs)}
 
 # Iterate over directories with a date format and which contain `dati-regioni`.
 directories = glob.glob('????-??-??/dati-regioni')
@@ -43,8 +45,7 @@ for directory in directories:
     for region in tqdm.tqdm(regions):
         
         # Prepare plot.
-        labels = ['total', 'infected']
-        for ax, label in zip(axs, labels):
+        for label, ax in axsl.items():
             ax.cla()
             ax.set_title(f'{region} ({label})')
         
@@ -52,12 +53,13 @@ for directory in directories:
         condition = data['denominazione_regione'] == region
         regiondata = data[condition]
         x = regiondata['data']
-        ys = {
+        ys_data = {
             'total': regiondata['totale_casi'],
-            'infected': regiondata['totale_attualmente_positivi']
+            'infected': regiondata['totale_attualmente_positivi'],
+            'removed': regiondata['totale_casi'] - regiondata['totale_attualmente_positivi']
         }
-        for ax, label in zip(axs, ys):
-            y = ys[label].values
+        for label, ax in axsl.items():
+            y = ys_data[label].values
             yerr = np.where(y > 0, np.sqrt(y), 1)
             ax.errorbar(x, y, yerr=yerr, label='data', marker='.', capsize=0, linestyle='')
         
@@ -65,7 +67,11 @@ for directory in directories:
         for filename, table in zip(files, tables):
             condition = table['denominazione_regione'] == region
             regiontable = table[condition]
+            
+            # times
             x = regiontable['data']
+            
+            # total and infected
             ys = {
                 'total': regiontable['totale_casi'],
                 'infected': regiontable['totale_attualmente_positivi']
@@ -74,24 +80,35 @@ for directory in directories:
                 'total': regiontable['std_totale_casi'],
                 'infected': regiontable['std_totale_attualmente_positivi']
             }
-            for ax, label in zip(axs, ys):
+            
+            # removed
+            try:
+                ys['removed'] = regiontable['guariti_o_deceduti']
+                yserr['removed'] = regiontable['std_guariti_o_deceduti']
+            except KeyError:
+                try:
+                    ys['removed'] = regiontable['dimessi_guariti'] + regiontable['deceduti']
+                    yserr['removed'] = np.hypot(regiontable['std_dimessi_guariti'], regiontable['std_deceduti'])
+                except KeyError:
+                    ys['removed'] = ys['total'] - ys['infected']
+                    yserr['removed'] = np.hypot(yserr['total'], yserr['infected'])
+            
+            # plot
+            for label, ax in axsl.items():
                 y = ys[label].values
                 yerr = yserr[label].values
                 nicename = os.path.splitext(os.path.split(filename)[-1])[0].replace('model-', '')
                 ax.errorbar(x, y, yerr=yerr, label=nicename, marker='', capsize=2, linestyle='')
         
         # Set smart logarithmic scale.
-        top = max([
-            regiondata[label].max()
-            for label in ['totale_casi', 'totale_attualmente_positivi']
-        ])
-        # top = 10 ** np.floor(np.log10(top))
-        top = max(1, top)
-        axs[0].set_yscale('symlog', linthreshy=top)
-        axs[0].yaxis.set_minor_locator(
-            symloglocator.MinorSymLogLocator(linthresh=top)
-        )
-        for ax in axs:
+        for label, ax in axsl.items():
+            top = ys_data[label].max()
+            # top = 10 ** np.floor(np.log10(top))
+            top = max(1, top)
+            ax.set_yscale('symlog', linthreshy=top)
+            ax.yaxis.set_minor_locator(
+                symloglocator.MinorSymLogLocator(linthresh=top)
+            )
             ax.axhline(top, linestyle='--', color='black', zorder=-1, label='logscale boundary')
         
         # Embellishments.

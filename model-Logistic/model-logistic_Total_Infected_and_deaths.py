9b9c1864a23cc6ee1aa60a8538f21c49c6669422
JacopoBusatto <62305286+JacopoBusatto@users.noreply.github.com>
2020-03-21 00:22:00 +0100

    chi2 and recovered updated

--- model-Logistic/model-logistic_Total_Infected_and_deaths.py
+++ model-Logistic/model-logistic_Total_Infected_and_deaths.py
@@ -11,11 +11,12 @@ from datetime import datetime,timedelta
 from sklearn.metrics import mean_squared_error
 from scipy.optimize import curve_fit
 from scipy.optimize import fsolve
+from scipy.stats import chisquare
 import matplotlib.pyplot as plt
 import csv 
 
 #Quality for plot saving
-DPI=600
+DPI=300
 
 # Define analytical model
 def logistic_model(x,a,b,c):
@@ -78,6 +79,9 @@ y_pred_exp =  [exponential_model(i,exp_fit[0][0], exp_fit[0][1],exp_fit[0][2]) f
 print('\nMSE logistic curve:    ',mean_squared_error(y,y_pred_logistic))
 # print('MSE exponential curve: ',mean_squared_error(y,y_pred_exp))
 
+DOF=float(len(x)-len(Par))
+chi2_logistic_infected = chisquare(y, [logistic(ii,Par) for ii in x])[0]/DOF
+print('chi2r = ', chi2_logistic_infected)
 
 
 
@@ -188,7 +192,7 @@ Xpredicted=[ii+max(x) for ii in range(1,NumberOfDaysPredicted+1)]
 Ypredicted=[logistic(ii,Par) for ii in Xpredicted]
 Ymin=[logistic(i,ParMAX) for i in xTOT]
 Ymax=[logistic(i,ParMIN) for i in xTOT]
-YPERR=[np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted]
+YPERR=np.array([np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted])*np.sqrt(chi2_logistic_infected)
 
 Ypredicted_logistic_infected=Ypredicted
 YPERR_logistic_infected=YPERR
@@ -276,7 +280,8 @@ y_pred_logistic = [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x]
 print('\nMSE logistic curve:    ',mean_squared_error(y,y_pred_logistic))
 # print('MSE exponential curve: ',mean_squared_error(y,y_pred_exp))
 
-
+chi2_logistic_deaths = chisquare(y, [logistic(ii,Par) for ii in x])[0]/DOF
+print('chi2 = ', chi2_logistic_deaths)
 
 
 
@@ -386,7 +391,7 @@ Xpredicted=[ii+max(x) for ii in range(1,NumberOfDaysPredicted+1)]
 Ypredicted=[logistic(ii,Par) for ii in Xpredicted]
 Ymin=[logistic(i,ParMAX) for i in xTOT]
 Ymax=[logistic(i,ParMIN) for i in xTOT]
-YPERR=[np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted]
+YPERR=np.array([np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted])*np.sqrt(chi2_logistic_deaths)
 
 
 
@@ -405,24 +410,236 @@ plt.ylabel("Total number of dead people")
 plt.ylim((min(y)*0.9,Par[2]*1.1))
 plt.grid(linestyle='--',which='both')
 plt.savefig('predictions_deaths.png', dpi=DPI)
+plt.show()
+
+Ypredicted_logistic_deaths=Ypredicted
+YPERR_logistic_deaths=YPERR
+
+
+
+
+
+
+#####################################################
+##### Faccio la stessa cosa per i guariti ###########
+#####################################################
+
+
+df = pd.read_csv(url)
+df = df.loc[:,['data','dimessi_guariti']]
+FMT = '%Y-%m-%d %H:%M:%S'
+date = df['data']
+df['data'] = date.map(lambda x : (datetime.strptime(x, FMT) - datetime.strptime("2020-01-01 00:00:00", FMT)).days  )
+
+x = list(df.iloc[:,0])
+y = list(df.iloc[:,1])
+YERR = np.sqrt(y)
+
+# Fitting Logistico
+P0=[0.2,70,10000]
+fit = curve_fit(logistic_model,x,y,P0, sigma=YERR, absolute_sigma=True)
+
+Par = [ii for ii in fit[0]]
+ErrPar = [np.sqrt(fit[1][ii][ii]) for ii in range(3)]
+
+print('\nLogistic fit parameters for recovered people')
+print('Rate of growth           =', fit[0][0])
+print('Peak day from Jan 1st    =', fit[0][1])
+print('Final number of recovered =', fit[0][2])
+
+sol = int(fsolve(lambda x : logistic(x,Par) - int(Par[2]),Par[1]))
+
+# Fitting esponenziale (Dummy, solo per grafico)
+exp_fit = curve_fit(exponential_model,x,y,p0=[0.005,0.17,50])
+
+pred_x = list(range(max(x),sol))
+xTOT= x+pred_x
+
+YMIN=np.array(y)-np.array(YERR)
+YMAX=np.array(y)+np.array(YERR)
+fitMIN = curve_fit(logistic_model,x,YMIN,p0=Par, sigma=YERR, absolute_sigma=True)
+fitMAX = curve_fit(logistic_model,x,YMAX,p0=Par, sigma=YERR, absolute_sigma=True)
+ParMAX=[fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]]
+ParMIN=[fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]]
+
+
+y_pred_logistic = [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x]
+y_pred_exp =  [exponential_model(i,exp_fit[0][0], exp_fit[0][1],exp_fit[0][2]) for i in x]
+print('\nMSE logistic curve:    ',mean_squared_error(y,y_pred_logistic))
+# print('MSE exponential curve: ',mean_squared_error(y,y_pred_exp))
+
+chi2_logistic_recovered = chisquare(y, [logistic(ii,Par) for ii in x])[0]/DOF
+print('chi2r = ', chi2_logistic_recovered)
+
+
+
+
+
+
+# Plot andamento
+plt.rcParams['figure.figsize'] = [9, 9]
+plt.rc('font', size=16)
+fig=plt.figure('linear_recovered')
+#Dati
+plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
+#Andamenti
+plt.plot(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
+plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='blue', alpha = 0.3 )
+plt.plot(xTOT, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in xTOT], label="Exponential model" )
+plt.legend()
+plt.xlabel("Days since 1 January 2020")
+plt.ylabel("Total number of recovered people")
+plt.ylim((min(y)*0.9,Par[2]*1.1))
+plt.grid(linestyle='--',which='both')
+plt.savefig('linear_recovered.png', dpi=DPI)
+#plt.show()
+
+#Log Plot
+fig=plt.figure('log_recovered')
+# Real data
+plt.grid()
+# plt.scatter(x,y,label="Real data",color="red")
+plt.errorbar(x, y, yerr=YERR, fmt='o',color="red",label="Data" )
+# Predicted logistic curve
+plt.semilogy(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
+# Predicted exponential curve
+plt.semilogy(x+pred_x, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in x+pred_x], label="Exponential model" )
+plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='red', alpha = 0.3 )
+plt.legend()
+plt.xlabel("Days since 1 January 2020")
+plt.ylabel("Total number of recovered people")
+plt.ylim((min(y)*0.9,Par[2]*1.1))
+plt.grid(linestyle='--',which='both')
+plt.savefig('log_recovered.png', dpi=DPI)
 
 #plt.show()
 
 
 
-Ypredicted_logistic_deaths=Ypredicted
-YPERR_logistic_deaths=YPERR
+
+# Ratio and differences
+Y2=np.array(y)
+Y1=np.array(y)
+Y1=np.delete(Y1,-1)
+Y1=np.insert(Y1,0,Y1[0])
+
+#Plot ratio
+fig=plt.figure('ratios_recovered')
+plt.plot(x,Y2/Y1,"--or",markersize=8,label="Ratio",alpha=0.6)
+plt.legend()
+plt.xlabel("Days since 1 January 2020")
+plt.ylabel("Ratio of recovered people respect to the day before")
+plt.ylim((min(Y2/Y1)*0.9,max(Y2/Y1)*1.1))
+plt.grid(linestyle='--',which='both')
+plt.savefig('ratios_recovered.png', dpi=DPI)
+#plt.show()
+
+
+# differences
+Y2=np.array(y)
+Y1=np.array(y)
+Y1=np.delete(Y1,-1)
+Y1=np.insert(Y1,0,Y1[0])
+Y3=Y2-Y1
+ERRY3=np.sqrt(Y2+Y1)
+X=np.array(x)
+X=np.delete(X,15,axis=0)
+Y3=np.delete(Y3,15,axis=0)
+ERRY3=np.delete(ERRY3,15,axis=0)
+fit_derivative = curve_fit(logistic_model_derivative,X,Y3,p0=[0.2,70,64000])
+fdM = curve_fit(logistic_model_derivative,X,Y3+ERRY3,p0=[0.2,70,64000])
+fdm = curve_fit(logistic_model_derivative,X,Y3-ERRY3,p0=[0.2,70,64000])
+Ymin= [logistic_derivative(i,fdm[0]) for i in list(X)+pred_x]
+Ymax= [logistic_derivative(i,fdM[0]) for i in list(X)+pred_x]
+
+
+# Real data
+fig=plt.figure('derivatives_recovered')
+#plt.scatter(X,Y3,label="Real data",color="red", alpha=0.6)
+plt.errorbar(X, Y3, yerr=ERRY3, fmt='o',color="red", alpha=0.75,label="Data" )
+# Predicted logistic curve
+plt.plot(list(X)+pred_x, [logistic_derivative(i,fit_derivative[0]) for i in list(X)+pred_x], label="Logistic model derivative" )
+plt.fill_between(list(X)+pred_x,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
+
+plt.legend()
+plt.xlabel("Days since 1 January 2020")
+plt.ylabel("Increase of recovered people")
+plt.ylim((min(Y3)*0.9,max([logistic_derivative(i,fit_derivative[0]) for i in list(X)+pred_x])*1.1))
+plt.grid(linestyle='--',which='both')
+plt.savefig('derivatives_recovered.png', dpi=DPI)
+
+#plt.show()
+
+
+
+
+# Predictions
+NumberOfDaysPredicted=14
+
+Xpredicted=[ii+max(x) for ii in range(1,NumberOfDaysPredicted+1)]
+Ypredicted=[logistic(ii,Par) for ii in Xpredicted]
+Ymin=[logistic(i,ParMAX) for i in xTOT]
+Ymax=[logistic(i,ParMIN) for i in xTOT]
+YPERR=np.array([np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted])*np.sqrt(chi2_logistic_recovered)
+
+Ypredicted_logistic_recovered=Ypredicted
+YPERR_logistic_recovered=YPERR
+
+
+
+
+
+
+#Plot with predictions
+fig=plt.figure('predictions_recovered')
+# plt.scatter(x,y,label="Real data",color="red",linestyle="None")
+plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
+plt.errorbar(Xpredicted,Ypredicted, yerr=YPERR, fmt='o',color="orange", alpha=1,label="Predictions ({} days)".format(NumberOfDaysPredicted) )
+
+# Predicted logistic curve
+plt.fill_between(x+pred_x, [logistic_model(i,fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]) for i in x+pred_x],[logistic_model(i,fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]) for i in x+pred_x],facecolor='blue', alpha = 0.3 )
+plt.plot(x+pred_x, [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x+pred_x], 'r',label="Logistic model" )
+
+plt.legend()
+plt.xlabel("Days since 1 January 2020")
+plt.ylabel("Total number of recovered people")
+plt.ylim((min(y)*0.9,Par[2]*1.1))
+plt.grid(linestyle='--',which='both')
+plt.savefig('predictions_recovered.png', dpi=DPI)
+#plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+totale_attualmente_positivi=np.array(Ypredicted_logistic_infected)-np.array(Ypredicted_logistic_recovered)-np.array(Ypredicted_logistic_deaths)
+std_totale_attualmente_positivi=np.sqrt(np.array(YPERR_logistic_infected)**2+np.array(YPERR_logistic_deaths)**2+np.array(YPERR_logistic_recovered)**2     )
 
 startingDate=737425
 Dates=[datetime.fromordinal(startingDate+ii) for ii in Xpredicted]
 for ii in range(len(Dates)):
     Dates[ii]= Dates[ii].replace(minute=00, hour=18,second=00)
 FirstLine=['denominazione_regione','data','totale_casi','std_totale_casi','totale_attualmente_positivi','std_totale_attualmente_positivi','deceduti','std_deceduti']
-with open('model-logistic_predictions.csv', 'w',newline='') as pred_logistic_file:
+with open('model-logistic-national.csv', 'w',newline='') as pred_logistic_file:
     wr = csv.writer(pred_logistic_file, quoting=csv.QUOTE_ALL)
     wr.writerow(FirstLine)
     for ii in range(len(Xpredicted)):
-        wr.writerow(['Italia',Dates[ii],Ypredicted_logistic_infected[ii],YPERR_logistic_infected[ii],'-','-',Ypredicted_logistic_deaths[ii],YPERR_logistic_deaths[ii]])
+        wr.writerow(['Italia',Dates[ii],Ypredicted_logistic_infected[ii],YPERR_logistic_infected[ii],totale_attualmente_positivi[ii],std_totale_attualmente_positivi[ii],Ypredicted_logistic_deaths[ii],YPERR_logistic_deaths[ii]])
+
+
 
 
 

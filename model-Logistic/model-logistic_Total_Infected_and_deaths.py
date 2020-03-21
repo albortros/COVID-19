ac54ccd380b068aeecc69bebd9f283997025efc4
JacopoBusatto <62305286+JacopoBusatto@users.noreply.github.com>
2020-03-21 13:38:02 +0100

    Updated errors evaluation

--- model-Logistic/model-logistic_Total_Infected_and_deaths.py
+++ model-Logistic/model-logistic_Total_Infected_and_deaths.py
@@ -7,7 +7,7 @@ Created on Fri Mar 20 02:22:58 2020
 
 import pandas as pd
 import numpy as np
-from datetime import datetime,timedelta
+from datetime import datetime
 from sklearn.metrics import mean_squared_error
 from scipy.optimize import curve_fit
 from scipy.optimize import fsolve
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 import csv 
 
 #Quality for plot saving
-DPI=300
+DPI=450
 
 # Define analytical model
 def logistic_model(x,a,b,c):
@@ -48,9 +48,10 @@ YERR = np.sqrt(y)
 
 # Fitting Logistico
 P0=[0.2,70,60000]
-fit = curve_fit(logistic_model,x,y,P0, sigma=YERR, absolute_sigma=True)
+fit = curve_fit(logistic_model,x,y,P0, sigma=YERR, absolute_sigma=False)
 
 Par = [ii for ii in fit[0]]
+Cov = fit[1]
 ErrPar = [np.sqrt(fit[1][ii][ii]) for ii in range(3)]
 
 print('\nLogistic fit parameters for total infected people')
@@ -66,15 +67,14 @@ exp_fit = curve_fit(exponential_model,x,y,p0=[0.005,0.17,50])
 pred_x = list(range(max(x),sol))
 xTOT= x+pred_x
 
-YMIN=np.array(y)-np.array(YERR)
-YMAX=np.array(y)+np.array(YERR)
-fitMIN = curve_fit(logistic_model,x,YMIN,p0=Par, sigma=YERR, absolute_sigma=True)
-fitMAX = curve_fit(logistic_model,x,YMAX,p0=Par, sigma=YERR, absolute_sigma=True)
-ParMAX=[fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]]
-ParMIN=[fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]]
+SIZE=100
+simulated_par= np.random.multivariate_normal(Par, Cov, size=SIZE)
+simulated_curve=[[logistic(ii,par) for ii in xTOT] for par in simulated_par]
+std_fit=np.std(simulated_curve, axis=0)
+Ymin=np.array([logistic(i,Par) for i in xTOT])-np.array(std_fit)
+Ymax=np.array([logistic(i,Par) for i in xTOT])+np.array(std_fit)
 
-
-y_pred_logistic = [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x]
+y_pred_logistic = [logistic(i,fit[0]) for i in x]
 y_pred_exp =  [exponential_model(i,exp_fit[0][0], exp_fit[0][1],exp_fit[0][2]) for i in x]
 print('\nMSE logistic curve:    ',mean_squared_error(y,y_pred_logistic))
 # print('MSE exponential curve: ',mean_squared_error(y,y_pred_exp))
@@ -97,7 +97,7 @@ fig=plt.figure('linear_infected')
 plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
 #Andamenti
 plt.plot(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
-plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='blue', alpha = 0.3 )
+plt.fill_between(xTOT,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.plot(xTOT, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in xTOT], label="Exponential model" )
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
@@ -117,7 +117,7 @@ plt.errorbar(x, y, yerr=YERR, fmt='o',color="red",label="Data" )
 plt.semilogy(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
 # Predicted exponential curve
 plt.semilogy(x+pred_x, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in x+pred_x], label="Exponential model" )
-plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='red', alpha = 0.3 )
+plt.fill_between(xTOT,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
 plt.ylabel("Total number of infected people")
@@ -159,11 +159,23 @@ X=np.array(x)
 X=np.delete(X,15,axis=0)
 Y3=np.delete(Y3,15,axis=0)
 ERRY3=np.delete(ERRY3,15,axis=0)
-fit_derivative = curve_fit(logistic_model_derivative,X,Y3,p0=[0.2,70,64000])
-fdM = curve_fit(logistic_model_derivative,X,Y3+ERRY3,p0=[0.2,70,64000])
-fdm = curve_fit(logistic_model_derivative,X,Y3-ERRY3,p0=[0.2,70,64000])
-Ymin= [logistic_derivative(i,fdm[0]) for i in list(X)+pred_x]
-Ymax= [logistic_derivative(i,fdM[0]) for i in list(X)+pred_x]
+fit_derivative = curve_fit(logistic_model_derivative,X,Y3,p0=[0.2,70,64000],sigma=ERRY3, absolute_sigma=False)
+ParD=fit_derivative[0]
+CovD=fit_derivative[1]
+simulated_par_D= np.random.multivariate_normal(ParD, CovD, size=SIZE)
+simulated_curve_D=[[logistic_derivative(ii,par) for ii in list(X)+pred_x] for par in simulated_par_D]
+std_fit_D=np.std(simulated_curve_D, axis=0)
+
+
+Ymin= np.array([logistic_derivative(i,ParD) for i in list(X)+pred_x])-np.array(std_fit_D)
+Ymax= np.array([logistic_derivative(i,ParD) for i in list(X)+pred_x])+np.array(std_fit_D)
+
+
+
+
+
+
+
 
 
 # Real data
@@ -187,12 +199,14 @@ plt.savefig('derivatives_infected.png', dpi=DPI)
 
 
 # Predictions
+
+
 NumberOfDaysPredicted=14
 Xpredicted=[ii+max(x) for ii in range(1,NumberOfDaysPredicted+1)]
 Ypredicted=[logistic(ii,Par) for ii in Xpredicted]
-Ymin=[logistic(i,ParMAX) for i in xTOT]
-Ymax=[logistic(i,ParMIN) for i in xTOT]
-YPERR=np.array([np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted])*np.sqrt(chi2_logistic_infected)
+Ymin=np.array([logistic(i,Par) for i in xTOT])-np.array(std_fit)
+Ymax=np.array([logistic(i,Par) for i in xTOT])+np.array(std_fit)
+YPERR=np.array([std_fit[i] for i in range(len(x),len(x)+NumberOfDaysPredicted)])
 
 Ypredicted_logistic_infected=Ypredicted
 YPERR_logistic_infected=YPERR
@@ -209,7 +223,7 @@ plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
 plt.errorbar(Xpredicted,Ypredicted, yerr=YPERR, fmt='o',color="orange", alpha=1,label="Predictions ({} days)".format(NumberOfDaysPredicted) )
 
 # Predicted logistic curve
-plt.fill_between(x+pred_x, [logistic_model(i,fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]) for i in x+pred_x],[logistic_model(i,fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]) for i in x+pred_x],facecolor='blue', alpha = 0.3 )
+plt.fill_between(xTOT,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.plot(x+pred_x, [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x+pred_x], 'r',label="Logistic model" )
 
 plt.legend()
@@ -249,9 +263,17 @@ YERR = np.sqrt(y)
 
 # Fitting Logistico
 P0=[0.3,70,5000]
-fit = curve_fit(logistic_model,x,y,P0, sigma=YERR, absolute_sigma=True)
+fit = curve_fit(logistic_model,x,y,P0, sigma=YERR, absolute_sigma=False)
+
+
+xTOT= x+pred_x
 
 Par = [ii for ii in fit[0]]
+Cov= fit[1]
+SIZE=100
+simulated_par= np.random.multivariate_normal(Par, Cov, size=SIZE)
+simulated_curve=[[logistic(ii,par) for ii in xTOT] for par in simulated_par]
+std_fit = np.std(simulated_curve, axis=0)
 ErrPar = [np.sqrt(fit[1][ii][ii]) for ii in range(3)]
 
 print('\nLogistic fit parameters for total deaths')
@@ -259,20 +281,12 @@ print('Rate of growth         =', fit[0][0])
 print('Peak day from Jan 1st  =', fit[0][1])
 print('Final number of deaths =', fit[0][2])
 
-sol = int(fsolve(lambda x : logistic(x,Par) - int(Par[2]),Par[1]))
 
 # Fitting esponenziale (Dummy, solo per grafico)
 exp_fit = curve_fit(exponential_model,x,y,p0=[0.005,0.17,50])
 
-pred_x = list(range(max(x),sol))
-xTOT= x+pred_x
 
-YMIN=np.array(y)-np.array(YERR)
-YMAX=np.array(y)+np.array(YERR)
-fitMIN = curve_fit(logistic_model,x,YMIN,p0=Par, sigma=YERR, absolute_sigma=True)
-fitMAX = curve_fit(logistic_model,x,YMAX,p0=Par, sigma=YERR, absolute_sigma=True)
-ParMAX=[fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]]
-ParMIN=[fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]]
+
 
 
 y_pred_logistic = [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x]
@@ -283,9 +297,8 @@ print('\nMSE logistic curve:    ',mean_squared_error(y,y_pred_logistic))
 chi2_logistic_deaths = chisquare(y, [logistic(ii,Par) for ii in x])[0]/DOF
 print('chi2 = ', chi2_logistic_deaths)
 
-
-
-
+Ymin=np.array([logistic(i,Par) for i in xTOT])-np.array(std_fit)
+Ymax=np.array([logistic(i,Par) for i in xTOT])+np.array(std_fit)
 
 
 # Plot andamento
@@ -294,7 +307,7 @@ fig=plt.figure('linear_deaths')
 plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
 #Andamenti
 plt.plot(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
-plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='blue', alpha = 0.3 )
+plt.fill_between(xTOT, Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 #plt.plot(xTOT, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in xTOT], label="Exponential model" )
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
@@ -302,8 +315,7 @@ plt.ylabel("Total number of dead people")
 plt.ylim((min(y)*0.9,Par[2]*1.1))
 plt.grid(linestyle='--',which='both')
 plt.savefig('linear_deaths.png', dpi=DPI)
-
-#plt.show()
+plt.show()
 
 #Log Plot
 fig=plt.figure('log_deaths')
@@ -314,15 +326,14 @@ plt.errorbar(x, y, yerr=YERR, fmt='o',color="red",label="Data" )
 # Predicted logistic curve
 plt.semilogy(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
 # Predicted exponential curve
-plt.semilogy(x+pred_x, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in x+pred_x], label="Exponential model" )
-plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='red', alpha = 0.3 )
+plt.semilogy(xTOT, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in xTOT], label="Exponential model" )
+plt.fill_between(xTOT, Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
 plt.ylabel("Total number of dead people")
 plt.ylim((min(y)*0.9,Par[2]*1.1))
 plt.grid(linestyle='--',which='both')
 plt.savefig('log_deaths.png', dpi=DPI)
-
 #plt.show()
 
 
@@ -358,11 +369,16 @@ X=np.array(x)
 X=np.delete(X,15,axis=0)
 Y3=np.delete(Y3,15,axis=0)
 ERRY3=np.delete(ERRY3,15,axis=0)
-fit_derivative = curve_fit(logistic_model_derivative,X,Y3,p0=[0.2,70,64000])
-fdM = curve_fit(logistic_model_derivative,X,Y3+ERRY3,p0=[0.2,70,64000])
-fdm = curve_fit(logistic_model_derivative,X,Y3-ERRY3,p0=[0.2,70,64000])
-Ymin= [logistic_derivative(i,fdm[0]) for i in list(X)+pred_x]
-Ymax= [logistic_derivative(i,fdM[0]) for i in list(X)+pred_x]
+fit_derivative = curve_fit(logistic_model_derivative,X,Y3,p0=[0.2,70,64000],sigma=ERRY3, absolute_sigma=False)
+ParD=fit_derivative[0]
+CovD=fit_derivative[1]
+simulated_par_D= np.random.multivariate_normal(ParD, CovD, size=SIZE)
+simulated_curve_D=[[logistic_derivative(ii,par) for ii in list(X)+pred_x] for par in simulated_par_D]
+std_fit_D=np.std(simulated_curve_D, axis=0)
+
+
+YminD= np.array([logistic_derivative(i,ParD) for i in list(X)+pred_x])-np.array(std_fit_D)
+YmaxD= np.array([logistic_derivative(i,ParD) for i in list(X)+pred_x])+np.array(std_fit_D)
 
 
 # Real data
@@ -371,7 +387,7 @@ fig=plt.figure('derivatives_deaths')
 plt.errorbar(X, Y3, yerr=ERRY3, fmt='o',color="red", alpha=0.75,label="Data" )
 # Predicted logistic curve
 plt.plot(list(X)+pred_x, [logistic_derivative(i,fit_derivative[0]) for i in list(X)+pred_x], label="Logistic model derivative" )
-plt.fill_between(list(X)+pred_x,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
+plt.fill_between(list(X)+pred_x,YminD,YmaxD,facecolor='blue', alpha = 0.3 )
 
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
@@ -389,11 +405,9 @@ plt.savefig('derivatives_deaths.png', dpi=DPI)
 NumberOfDaysPredicted=14
 Xpredicted=[ii+max(x) for ii in range(1,NumberOfDaysPredicted+1)]
 Ypredicted=[logistic(ii,Par) for ii in Xpredicted]
-Ymin=[logistic(i,ParMAX) for i in xTOT]
-Ymax=[logistic(i,ParMIN) for i in xTOT]
-YPERR=np.array([np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted])*np.sqrt(chi2_logistic_deaths)
-
-
+Ymin=np.array([logistic(i,Par) for i in xTOT])-np.array(std_fit)
+Ymax=np.array([logistic(i,Par) for i in xTOT])+np.array(std_fit)
+YPERR=np.array([std_fit[i] for i in range(len(x),len(x)+NumberOfDaysPredicted)])
 
 
 #Plot with predictions
@@ -402,7 +416,7 @@ fig=plt.figure('predictions_deaths')
 plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
 plt.errorbar(Xpredicted,Ypredicted, yerr=YPERR, fmt='o',color="orange", alpha=1,label="Predictions ({} days)".format(NumberOfDaysPredicted) )
 # Predicted logistic curve
-plt.fill_between(x+pred_x, [logistic_model(i,fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]) for i in x+pred_x],[logistic_model(i,fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]) for i in x+pred_x],facecolor='blue', alpha = 0.3 )
+plt.fill_between(xTOT,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.plot(x+pred_x, [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x+pred_x], 'r',label="Logistic model" )
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
@@ -437,9 +451,10 @@ YERR = np.sqrt(y)
 
 # Fitting Logistico
 P0=[0.2,70,10000]
-fit = curve_fit(logistic_model,x,y,P0, sigma=YERR, absolute_sigma=True)
+fit = curve_fit(logistic_model,x,y,P0, sigma=YERR, absolute_sigma=False)
 
 Par = [ii for ii in fit[0]]
+Cov = fit[1]
 ErrPar = [np.sqrt(fit[1][ii][ii]) for ii in range(3)]
 
 print('\nLogistic fit parameters for recovered people')
@@ -452,15 +467,13 @@ sol = int(fsolve(lambda x : logistic(x,Par) - int(Par[2]),Par[1]))
 # Fitting esponenziale (Dummy, solo per grafico)
 exp_fit = curve_fit(exponential_model,x,y,p0=[0.005,0.17,50])
 
-pred_x = list(range(max(x),sol))
 xTOT= x+pred_x
 
-YMIN=np.array(y)-np.array(YERR)
-YMAX=np.array(y)+np.array(YERR)
-fitMIN = curve_fit(logistic_model,x,YMIN,p0=Par, sigma=YERR, absolute_sigma=True)
-fitMAX = curve_fit(logistic_model,x,YMAX,p0=Par, sigma=YERR, absolute_sigma=True)
-ParMAX=[fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]]
-ParMIN=[fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]]
+simulated_par= np.random.multivariate_normal(Par, Cov, size=SIZE)
+simulated_curve=[[logistic(ii,par) for ii in xTOT] for par in simulated_par]
+std_fit=np.std(simulated_curve, axis=0)
+Ymin=np.array([logistic(i,Par) for i in xTOT])-np.array(std_fit)
+Ymax=np.array([logistic(i,Par) for i in xTOT])+np.array(std_fit)
 
 
 y_pred_logistic = [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x]
@@ -484,7 +497,7 @@ fig=plt.figure('linear_recovered')
 plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
 #Andamenti
 plt.plot(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
-plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='blue', alpha = 0.3 )
+plt.fill_between(xTOT,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.plot(xTOT, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in xTOT], label="Exponential model" )
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
@@ -504,7 +517,7 @@ plt.errorbar(x, y, yerr=YERR, fmt='o',color="red",label="Data" )
 plt.semilogy(xTOT, [logistic(i,Par) for i in xTOT], label="Logistic model" )
 # Predicted exponential curve
 plt.semilogy(x+pred_x, [exponential_model(i,exp_fit[0][0],exp_fit[0][1],exp_fit[0][2]) for i in x+pred_x], label="Exponential model" )
-plt.fill_between(xTOT, [logistic(i,ParMAX) for i in xTOT],[logistic(i,ParMIN) for i in xTOT],facecolor='red', alpha = 0.3 )
+plt.fill_between(xTOT,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
 plt.ylabel("Total number of recovered people")
@@ -546,11 +559,16 @@ X=np.array(x)
 X=np.delete(X,15,axis=0)
 Y3=np.delete(Y3,15,axis=0)
 ERRY3=np.delete(ERRY3,15,axis=0)
-fit_derivative = curve_fit(logistic_model_derivative,X,Y3,p0=[0.2,70,64000])
-fdM = curve_fit(logistic_model_derivative,X,Y3+ERRY3,p0=[0.2,70,64000])
-fdm = curve_fit(logistic_model_derivative,X,Y3-ERRY3,p0=[0.2,70,64000])
-Ymin= [logistic_derivative(i,fdm[0]) for i in list(X)+pred_x]
-Ymax= [logistic_derivative(i,fdM[0]) for i in list(X)+pred_x]
+fit_derivative = curve_fit(logistic_model_derivative,X,Y3,p0=[0.2,70,64000],sigma=ERRY3, absolute_sigma=False)
+ParD=fit_derivative[0]
+CovD=fit_derivative[1]
+simulated_par_D= np.random.multivariate_normal(ParD, CovD, size=SIZE)
+simulated_curve_D=[[logistic_derivative(ii,par) for ii in list(X)+pred_x] for par in simulated_par_D]
+std_fit_D=np.std(simulated_curve_D, axis=0)
+
+
+YminD= np.array([logistic_derivative(i,ParD) for i in list(X)+pred_x])-np.array(std_fit_D)
+YmaxD= np.array([logistic_derivative(i,ParD) for i in list(X)+pred_x])+np.array(std_fit_D)
 
 
 # Real data
@@ -559,7 +577,7 @@ fig=plt.figure('derivatives_recovered')
 plt.errorbar(X, Y3, yerr=ERRY3, fmt='o',color="red", alpha=0.75,label="Data" )
 # Predicted logistic curve
 plt.plot(list(X)+pred_x, [logistic_derivative(i,fit_derivative[0]) for i in list(X)+pred_x], label="Logistic model derivative" )
-plt.fill_between(list(X)+pred_x,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
+plt.fill_between(list(X)+pred_x,YminD,YmaxD,facecolor='blue', alpha = 0.3 )
 
 plt.legend()
 plt.xlabel("Days since 1 January 2020")
@@ -575,12 +593,11 @@ plt.savefig('derivatives_recovered.png', dpi=DPI)
 
 # Predictions
 NumberOfDaysPredicted=14
-
+Ymin=np.array([logistic(i,Par) for i in xTOT])-np.array(std_fit)
+Ymax=np.array([logistic(i,Par) for i in xTOT])+np.array(std_fit)
 Xpredicted=[ii+max(x) for ii in range(1,NumberOfDaysPredicted+1)]
 Ypredicted=[logistic(ii,Par) for ii in Xpredicted]
-Ymin=[logistic(i,ParMAX) for i in xTOT]
-Ymax=[logistic(i,ParMIN) for i in xTOT]
-YPERR=np.array([np.absolute(logistic(i,ParMAX)-logistic(i,ParMIN))/2. for i in Xpredicted])*np.sqrt(chi2_logistic_recovered)
+YPERR=np.array([std_fit[i] for i in range(len(x),len(x)+NumberOfDaysPredicted)])
 
 Ypredicted_logistic_recovered=Ypredicted
 YPERR_logistic_recovered=YPERR
@@ -597,7 +614,7 @@ plt.errorbar(x, y, yerr=YERR, fmt='o',color="red", alpha=0.75,label="Data" )
 plt.errorbar(Xpredicted,Ypredicted, yerr=YPERR, fmt='o',color="orange", alpha=1,label="Predictions ({} days)".format(NumberOfDaysPredicted) )
 
 # Predicted logistic curve
-plt.fill_between(x+pred_x, [logistic_model(i,fitMAX[0][0],fitMAX[0][1],fitMAX[0][2]) for i in x+pred_x],[logistic_model(i,fitMIN[0][0],fitMIN[0][1],fitMIN[0][2]) for i in x+pred_x],facecolor='blue', alpha = 0.3 )
+plt.fill_between(xTOT,Ymin,Ymax,facecolor='blue', alpha = 0.3 )
 plt.plot(x+pred_x, [logistic_model(i,fit[0][0],fit[0][1],fit[0][2]) for i in x+pred_x], 'r',label="Logistic model" )
 
 plt.legend()

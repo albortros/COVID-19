0e5c3fd5a1ee9b5c687ab98488ed92967dc9ae4a
Giacomo Petrillo <info@giacomopetrillo.com>
2020-03-31 23:23:20 +0200

    generalized run-models.py for non-directory models

--- run-models.py
+++ run-models.py
@@ -16,10 +16,12 @@ def chdir(dir):
     """
     prev_dir = os.getcwd()
     os.chdir(dir)
+    print(f'moved to `{dir}/`')
     try:
         yield os.getcwd()
     finally:
         os.chdir(prev_dir)
+        print(f'moved out of `{dir}/` to `{prev_dir}/`')
 
 def command(cmd):
     """
@@ -40,47 +42,45 @@ def eprint(*args):
 model_commands = dict() # key = model directory, value = code
 
 model_commands['model-SIR-by-region-bayes'] = """
-command('python3 fitlsq.py')
-files = glob.glob('fitlsq_*UTC.pickle')
-files.sort()
-file = files[-1]
-# command(f'python3 fitlsqplot.py {file}')
-command(f'python3 fitlsqpred.py {file}')
+with chdir('model-SIR-by-region-bayes'):
+    command('python3 fitlsq.py weakpop')
+    files = glob.glob('fitlsq_*UTC.pickle')
+    files.sort()
+    file = files[-1]
+    command(f'python3 fitlsqplot.py {file}')
+    command(f'python3 fitlsqpred.py {file}')
 """
 
-model_commands['model-SIR-region-truepop'] = model_commands['model-SIR-by-region-bayes']
+model_commands['model-SIR-region-truepop'] = model_commands['model-SIR-by-region-bayes'].replace('weakpop', 'truepop')
 
 model_commands['model-Logistic'] = """
-url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
-df = pd.read_csv(url, parse_dates=['data'])
-lastdate = df['data'].max()
-savedir = f'../predictions/{lastdate.year:04d}-{lastdate.month:02d}-{lastdate.day:02d}/dati-andamento-nazionale'
-os.makedirs(savedir, exist_ok=True)
-os.makedirs('Plots', exist_ok=True)
-command('python3 model-logistic.py')
-filename = 'model-logistic-national.csv'
-shutil.copy(filename, f'{savedir}/{filename}')
+with chdir('model-Logistic'):
+    url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+    df = pd.read_csv(url, parse_dates=['data'])
+    lastdate = df['data'].max()
+    savedir = f'../predictions/{lastdate.year:04d}-{lastdate.month:02d}-{lastdate.day:02d}/dati-andamento-nazionale'
+    os.makedirs(savedir, exist_ok=True)
+    os.makedirs('Plots', exist_ok=True)
+    command('python3 model-logistic.py')
+    filename = 'model-logistic-national.csv'
+    shutil.copy(filename, f'{savedir}/{filename}')
 """
 
-model_commands['model-Gompertz'] = model_commands['model-Logistic'].replace('logistic', 'gompertz')
+model_commands['model-Gompertz'] = model_commands['model-Logistic'].replace('logistic', 'gompertz').replace('Logistic', 'Gompertz')
 
 if sys.argv[1:]:
-    model_dirs = sys.argv[1:]
+    models = sys.argv[1:]
 else:
-    model_dirs = sorted(model_commands.keys())
+    models = sorted(model_commands.keys())
 
-for modeldir in model_dirs:
-    print(f'--------------- {modeldir} ---------------')
-    if not os.path.isdir(modeldir):
-        eprint(f'`{modeldir}` is not a directory, skipping')
-        continue
-    if not modeldir in model_commands:
-        eprint(f'code not available for `{modeldir}`, skipping')
+for model in models:
+    print(f'--------------- {model} ---------------')
+    if not model in model_commands:
+        eprint(f'code not available for `{model}`, skipping')
         continue
     
     try:
-        with chdir(modeldir):
-            exec(model_commands[modeldir])
+        exec(model_commands[model])
     except Exception as exc:
         eprint(f'There was an error running the commands:\n{type(exc).__name__}({", ".join(map(str, exc.args))})')
 

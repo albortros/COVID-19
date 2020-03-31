e29ce9376ad8aa244a3535a98fe22026cca261f1
Giacomo Petrillo <info@giacomopetrillo.com>
2020-04-01 00:17:07 +0200

    run-models.py sets env variable with data date

--- run-models.py
+++ run-models.py
@@ -7,6 +7,11 @@ import glob
 import sys
 import pandas as pd
 import shutil
+import argparse
+import numpy as np
+
+def daystring(timestamp):
+    return f'{timestamp.year:04d}-{timestamp.month:02d}-{timestamp.day:02d}'
 
 @contextlib.contextmanager
 def chdir(dir):
@@ -23,15 +28,31 @@ def chdir(dir):
         os.chdir(prev_dir)
         print(f'moved out of `{dir}/` to `{prev_dir}/`')
 
-def command(cmd):
+command_default_env = dict()
+def command(cmd, env=None):
     """
     Print a command and run it. Raise SystemError if the command returns
     nonzero.
     """
+    cmdname = cmd.split()[0]
+
+    global command_default_env
+    if env is None:
+        env = command_default_env
+    else:
+        newenv = command_default_env.copy()
+        newenv.update(env)
+        env = newenv
+    
+    precmds = ''
+    for var, value in env.items():
+        precmds += f'{var}={value} '
+    cmd = precmds + cmd
+    
     print('command:', cmd)
     ret = os.system(cmd)
     if ret:
-        raise SystemError('command `{}` returned {}'.format(cmd.split()[0], ret))
+        raise SystemError('command `{}` returned {}'.format(cmdname, ret))
 
 errors = 0
 def eprint(*args):
@@ -47,7 +68,7 @@ with chdir('model-SIR-by-region-bayes'):
     files = glob.glob('fitlsq_*UTC.pickle')
     files.sort()
     file = files[-1]
-    command(f'python3 fitlsqplot.py {file}')
+    # command(f'python3 fitlsqplot.py {file}')
     command(f'python3 fitlsqpred.py {file}')
 """
 
@@ -68,21 +89,42 @@ with chdir('model-Logistic'):
 
 model_commands['model-Gompertz'] = model_commands['model-Logistic'].replace('logistic', 'gompertz').replace('Logistic', 'Gompertz')
 
-if sys.argv[1:]:
-    models = sys.argv[1:]
+# Parse command line.
+parser = argparse.ArgumentParser(
+    description='Run models to compute predictions.'
+)
+parser.add_argument('-d', '--date', help='last date to use for data (e.g. 2020-XX-XX)', action='append', nargs='+')
+parser.add_argument('-m', '--model', help='model (e.g. gompertz, abbreviations allowed)', action='append', nargs='+')
+args = parser.parse_args()
+
+# Dates for data used.
+if getattr(args, 'date', None):
+    lastdates = np.concatenate(args.date)
+    lastdates = [pd.Timestamp(str(date)) for date in lastdates]
+else:
+    lastdates = [pd.Timestamp.today()]
+
+# Models.
+if getattr(args, 'model', None):
+    models = np.concatenate(args.model)
 else:
     models = sorted(model_commands.keys())
 
 for model in models:
     print(f'--------------- {model} ---------------')
+    
     if not model in model_commands:
         eprint(f'code not available for `{model}`, skipping')
         continue
+
+    for lastdate in lastdates:
+        print(f'--- using data up to {daystring(lastdate)} ---')
     
-    try:
-        exec(model_commands[model])
-    except Exception as exc:
-        eprint(f'There was an error running the commands:\n{type(exc).__name__}({", ".join(map(str, exc.args))})')
+        try:
+            command_default_env['LASTDATE'] = daystring(lastdate)
+            exec(model_commands[model])
+        except Exception as exc:
+            eprint(f'There was an error running the commands:\n{type(exc).__name__}({", ".join(map(str, exc.args))})')
 
 end = time.time()
 interval = pd.Timedelta(end - start, 'sec')

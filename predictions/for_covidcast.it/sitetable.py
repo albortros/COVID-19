457f1d7a5c7cb82cb45cfe71d4a04ef17a1a28c0
Giacomo Petrillo <info@giacomopetrillo.com>
2020-04-09 16:50:35 +0200

    site table with time switch

--- predictions/for_covidcast.it/sitetable.py
+++ predictions/for_covidcast.it/sitetable.py
@@ -20,29 +20,56 @@ grouped = csv.groupby('codice_regione')
 output = ''
 for code in csv['codice_regione'].unique():
     row = grouped.get_group(code)
+    
+    # id for the outer div.
     region = row['denominazione_regione'].values[0]
     if code == 4:
         region = 'Trentino-Alto Adige'
     regionid = 'table' + region.replace("'", '').replace(' ','').replace('-', '').lower()
+    
+    today = row['data'].max()
+    midnight = pd.Timestamp(year=today.year, month=today.month, day=today.day + 1, hour=0, minute=0, second=0)
+    
     output += f'''
 <div id="{regionid}" class="predtablediv">
-    <table>
-        <tr class="header">
-            <td class="leftheader">{region}</td>
-            <td>oggi</td>
-            <td>domani</td>
-        </tr>
-        <tr>
-            <td class="leftheader">casi</td>
-            <td>{row['casi_domani'].sum():+.0f}</td>
-            <td>{row['casi_dopodomani'].sum():+.0f}</td>
-        </tr>
-        <tr>
-            <td class="leftheader">deceduti</td>
-            <td>{row['morti_domani'].sum():+.0f}</td>
-            <td>{row['morti_dopodomani'].sum():+.0f}</td>
-        </tr>
-    </table>
+    <div id="{today}" class="timeswitch">
+        <table>
+            <tr class="header">
+                <td class="leftheader">{region}</td>
+                <td>oggi (dati)</td>
+                <td>domani</td>
+            </tr>
+            <tr>
+                <td class="leftheader">casi</td>
+                <td>{row['casi_oggi'].sum():+.0f}</td>
+                <td>{row['casi_domani'].sum():+.0f}</td>
+            </tr>
+            <tr>
+                <td class="leftheader">deceduti</td>
+                <td>{row['morti_oggi'].sum():+.0f}</td>
+                <td>{row['morti_domani'].sum():+.0f}</td>
+            </tr>
+        </table>
+    </div>
+    <div id="{midnight}" class="timeswitch">
+        <table>
+            <tr class="header">
+                <td class="leftheader">{region}</td>
+                <td>oggi (pred.)</td>
+                <td>domani</td>
+            </tr>
+            <tr>
+                <td class="leftheader">casi</td>
+                <td>{row['casi_domani'].sum():+.0f}</td>
+                <td>{row['casi_dopodomani'].sum():+.0f}</td>
+            </tr>
+            <tr>
+                <td class="leftheader">deceduti</td>
+                <td>{row['morti_domani'].sum():+.0f}</td>
+                <td>{row['morti_dopodomani'].sum():+.0f}</td>
+            </tr>
+        </table>
+    </div>
 </div>
 '''
 output = output.lstrip()
